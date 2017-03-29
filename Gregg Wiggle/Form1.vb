e6093a95e51183a154879f1dd9353de4d31bf420
colenthriwolf <dogesoft@outlook.com>
2017-03-28 20:23:34 -0700

    Made it smaller

--- Gregg Wiggle/Form1.vb
+++ Gregg Wiggle/Form1.vb
@@ -1,11 +1,7 @@
 ﻿Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'Gregg wiggle
-        '     -Johnny "colenthriwolf" Stene
         Me.TopMost = True
-        Me.SetStyle(ControlStyles.SupportsTransparentBackColor, True)
-        Me.BackColor = Color.Transparent
     End Sub
 
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
